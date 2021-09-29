5f7bdc558e61e015b241fbbb12fdbd52eb3acffa
kuole-o <63436555+kuole-o@users.noreply.github.com>
2021-09-29 14:45:04 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,40 +3,55 @@
 # @Date      : 2021/6/5 10:16
 
 import json
+import time
 import requests
-from bs4 import BeautifulSoup
 from http.server import BaseHTTPRequestHandler
 
 
 def get_data():
     """微博热搜
-
     Args:
         params (dict): {}
-
     Returns:
         json: {title: 标题, url: 地址, num: 热度数值, hot: 热搜等级}
     """
 
     data = []
-    response = requests.get('https://s.weibo.com/top/summary/')
-    soup = BeautifulSoup(response.text, 'html.parser')
-    url = soup.select('#pl_top_realtimehot > table > tbody > tr > td.td-02 > a')
-    num = soup.select('#pl_top_realtimehot > table > tbody > tr > td.td-02 > span')
-    hot = soup.select('#pl_top_realtimehot > table > tbody > tr > td.td-03')
+    t = time.time()
+    timestamp = str(round(t * 10000))
+    url = 'https://s.weibo.com/ajax/jsonp/gettopsug?_cb=STK_' + timestamp
+    response = requests.get(url)
+    # 返回js
+    data_str = response.text.replace(')}catch(e){}', ''). \
+        replace('try{window.STK_' + timestamp + '&STK_' + timestamp + '(', '')
+    data_dict = json.loads(data_str)
+    jyzy = {
+        '电影': '影',
+        '剧集': '剧',
+        '综艺': '综',
+        '音乐': '音'
+    }
 
-    for i in range(1, len(url)):
-        news = {
-            'title': url[i].get_text(),
-            'url': "https://s.weibo.com" + url[i]['href'],
-            'num': num[i - 1].get_text()
+    for data_item in data_dict['data']['list']:
+        hot = ''
+        if 'flag_desc' in data_item:
+            hot = jyzy.get(data_item['flag_desc'])
+        if 'is_boom' in data_item:
+            hot = '爆'
+        if 'is_hot' in data_item:
+            hot = '热'
+        if 'is_fei' in data_item:
+            hot = '沸'
+        if 'is_new' in data_item:
+            hot = '新'
+
+        dic = {
+            'title': data_item['note'],
+            'url': 'https://s.weibo.com/weibo?q=' + data_item['word'].replace('#', '%23') + '&Refer=top',
+            'num': data_item['num'],
+            'hot': hot
         }
-        hotness = hot[i].get_text().replace('</i>', ''). \
-            replace('<i class="icon-txt icon-txt-hot">', '').replace('<i class="icon-txt icon-txt-new">', '')
-        news['hot'] = hotness
-        # 去除广告链接
-        if hotness != '荐' and hotness != '商':
-            data.append(news)
+        data.append(dic)
 
     return data
 
@@ -51,3 +66,7 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(json.dumps(data).encode('utf-8'))
         return
+
+
+if __name__ == '__main__':
+    print(get_data())
